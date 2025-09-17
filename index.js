800fdc4bf5dbc0d8a71d686dffa2e504fa5e7d45
SurendraReddy53 <42991746+SurendraReddy53@users.noreply.github.com>
2025-09-17 22:06:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello from CI/CD Pipeline');
+
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
